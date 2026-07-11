468a9c50988777492813456380d1a173f7e99abe
vat120-wq <vat120@gmail.com>
2026-07-11 13:39:38 -0600

    Refresh cached auth configuration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='trufit-v8',ASSETS=['./','./index.html','./styles.css','./app.js','./cloud-config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png','./icons/favicon-16.png','./icons/favicon.ico'];
+const CACHE='trufit-v9',ASSETS=['./','./index.html','./styles.css','./app.js','./cloud-config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png','./icons/favicon-16.png','./icons/favicon.ico'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
